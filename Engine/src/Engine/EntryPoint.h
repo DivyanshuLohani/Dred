81f07a5893fb5088aa251948129c548095e66851
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2023-01-27 22:35:59 +0530

    Add premake and logging

--- Engine/src/Engine/EntryPoint.h
+++ Engine/src/Engine/EntryPoint.h
@@ -1,10 +1,14 @@
 #pragma once
 #ifdef ENGINE_WINDOWS_BUILD
 #include "Application.h"	
+#include "Log.h"	
 
 extern Engine::Application* application;
 
 void main(int argc, char** argv) {
+	Engine::Log::Init();
+	EN_CORE_WARN("Initialize Core Logger");
+	EN_INFO("Initialize Client Logger");
 	auto app = Engine::CreateApplication();
 	app->Run();
 	delete app;
